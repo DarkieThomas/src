1d54822482aca91b0b90b92f112683a16306195a
DarkieThomas <frostsaibedinov@gmail.com>
2026-05-10 03:06:04 +0700

    Update bringup.launch.py

--- bringup.launch.py
+++ bringup.launch.py
@@ -79,3 +79,76 @@ def generate_launch_description():
         ),
 
     ])
+
+
+
+
+
+
+
+//
+
+
+
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+from ament_index_python.packages import get_package_share_directory
+
+import os
+import xacro
+
+
+def generate_launch_description():
+
+    pkg_path = get_package_share_directory('darkie')
+
+    xacro_file = os.path.join(
+        pkg_path,
+        'urdf',
+        'maze_urdf.xacro'
+    )
+
+    robot_description_config = xacro.process_file(xacro_file)
+
+    robot_description = {
+        'robot_description': robot_description_config.toxml()
+    }
+
+    controller_config = os.path.join(
+        pkg_path,
+        'config',
+        'controllers.yaml'
+    )
+
+    return LaunchDescription([
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            output='screen',
+            parameters=[robot_description]
+        ),
+
+        Node(
+            package='controller_manager',
+            executable='ros2_control_node',
+            output='screen',
+            parameters=[controller_config]
+        ),
+
+        Node(
+            package='controller_manager',
+            executable='spawner',
+            arguments=['joint_state_broadcaster'],
+            output='screen'
+        ),
+
+        Node(
+            package='controller_manager',
+            executable='spawner',
+            arguments=['diff_drive_controller'],
+            output='screen'
+        ),
+
